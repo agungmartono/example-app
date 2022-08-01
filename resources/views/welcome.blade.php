18f41ffbd32c2e5ebc9620aeb5f23dc70d40972f
Agung Martono <39522446+agungmartono@users.noreply.github.com>
2022-08-01 11:20:28 +0700

    dqwdqwd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,8 +125,9 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 				@php
-					Nama = {{ App\Models\User::find(1)->name }};
+					$data= {{ App\Models\User::find(1)->name }};
 				@endphp
+                    Nama = $data;
                     </div>
                     
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
