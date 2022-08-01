5bee809d89b1008dc6dd5b5c9c9ec9dfd09aa16a
Agung Martono <39522446+agungmartono@users.noreply.github.com>
2022-08-01 11:27:13 +0700

    dwqd

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Models\User;
 
 Route::get('/', function () {
     $data = User::find(1);
-    dd($data->name);
-    return view('welcome', ['data' => $data]);
+    return view('welcome', [
+        'data' => $data
+    ]);
 });
