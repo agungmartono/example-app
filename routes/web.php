278348eccab3a8214619db86fe29895517c77285
Agung Martono <39522446+agungmartono@users.noreply.github.com>
2022-08-01 11:22:30 +0700

    dqdqwd

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = \App\Models\User::find(1);
+    
+    return view('welcome', ['$data' => $data]););
 });
